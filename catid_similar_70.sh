4a6c7938c0bba2c905edd440415c6bcc5c83a96e
catid <mrcatid@gmail.com>
2024-04-22 23:55:49 +0000

    wip

--- catid_similar_70.sh
+++ catid_similar_70.sh
@@ -2,7 +2,7 @@ python ./compute_block_similarity/layer_similarity.py \
     --model_path "Meta-Llama-3-70B-Instruct" \
     --dataset "arcee-ai/sec-data-mini" \
     --dataset_column "text" \
-    --batch_size 8 \
+    --batch_size 4 \
     --max_length 8192 \
     --layers_to_skip 8 \
     --dataset_size 4000 \
